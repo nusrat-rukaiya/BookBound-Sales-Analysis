4aaac7df5ba9d2b8feca3f944fae18fc7f580195
nusrat-rukaiya <rukaiyanusrat76@gmail.com>
2026-05-12 10:59:44 +0600

    new

--- SQL file.sql
+++ SQL file.sql
@@ -1,5 +1,17 @@
 -- ============================================
 -- BOOKBOUND SALES ANALYSIS
 -- SQL Portfolio Project
--- Author: Your Name
+-- Author: Nusrat Ruakiya
 -- ============================================
+
+-- Total Revenue ,Number of Orders and Unique Customers
+
+SELECT
+    SUM(total_amount) AS revenue,
+    COUNT(order_id) AS total_orders,
+    COUNT(DISTINCT customer_id) AS total_customers
+FROM orders;
+
+| Revenue | Total Orders | Total Customers |
+| ------- | ------------ | --------------- |
+| 75628.66| 500          | 307             |
