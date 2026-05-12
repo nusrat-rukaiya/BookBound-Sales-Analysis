ae9163de1224bbfeb911692714e75353741e568f
nusrat-rukaiya <rukaiyanusrat76@gmail.com>
2026-05-13 00:09:03 +0600

    new post

--- SQL file.sql
+++ SQL file.sql
@@ -15,3 +15,46 @@ FROM orders;
 | Revenue | Total Orders | Total Customers |
 | ------- | ------------ | --------------- |
 | 75628.66| 500          | 307             |
+
+
+-- Total Stock by Genre
+SELECT 
+    genre,
+    SUM(stock) AS total_stock
+FROM books
+GROUP BY genre;
+
+| Genre          | Total Stock |
+| -------------- | ----------- |
+| Romance        | 3540        |
+| Biography      | 3149        |
+| Mystery        | 3932        |
+| Fantasy        | 3764        |
+| Fiction        | 3049        |
+| Non-Fiction    | 3574        |
+| Science Fiction| 4048        |
+
+-- Book with the lowest and highest stock
+SELECT title, stock
+FROM books
+WHERE stock IN (
+    (SELECT MIN(stock) FROM books),
+    (SELECT MAX(stock) FROM books)
+);
+
+| Title                                         | Stock |
+|-----------------------------------------------|-------|
+| "Configurable modular throughput"             | 100   |
+| "Quality-focused multi-tasking challenge"     | 100   |
+| "Synergistic dedicated concept"               | 100   |
+| "Networked systemic implementation"           | 0     |
+| "Robust eco-centric capacity"                 | 0     |
+| "Business-focused real-time benchmark"        | 0     |
+| "Object-based eco-centric challenge"          | 0     |
+| "Ergonomic foreground Graphic Interface"      | 100   |
+| "Customer-focused tertiary methodology"       | 100   |
+| "Team-oriented bandwidth-monitored project"   | 100   |
+| "Diverse responsive focus group"              | 100   |
+| "Future-proofed heuristic function"           | 0     |
+| "Stand-alone zero administration emulation"   | 100   |
+
